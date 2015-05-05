d078a971765f7a0d9559bbee1addfff46c22917b
Eduardo F. Casal <educasal@users.noreply.github.com>
2015-05-05 23:34:27 +0200

    First steps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,93 @@
 'use strict';
 
 module.exports = function (grunt) {
-	
-	require('time-grunt')(grunt);
-	
-	require('load-grunt-tasks')(grunt, {
-		scope: 'devDependencies',
-		config: 'package.json',
-		pattern: ['grunt-*']
-	});
-	
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			all: [
-				'src/**/*.js',
-				'example/app.js',
-				'Gruntfile.js'
-			]
-		},
-		uglify: {
-			options: {
-				mangle: false,
-				sourceMap: true,
-				sourceMapName: 'dist/processing-yeoman.min.map'
-			},
-			myTarget: {
-				files: {
-					'dist/processing-yeoman.min.js': ['src/processing-yeoman.js']
-				}
-			}
-		},
-		copy: {
-			main: {
-				files: [
-					{src: ['src/processing-yeoman.js'], dest: 'dist/processing-yeoman.js'},
-					{src: ['dist/processing-yeoman.min.js'], dest: 'example/processing-yeoman.min.js'},
-					{src: ['dist/processing-yeoman.min.map'], dest: 'example/processing-yeoman.min.map'},
-					{src: ['bower_components/jquery/jquery.min.js'], dest: 'example/jquery.min.js'},
-					{src: ['bower_components/jquery/jquery.min.map'], dest: 'example/jquery.min.map'}
-				]
-			}
-		},
-		jsdoc : {
-			dist : {
-				src: ['src/**/*.js'],
-				options: {
-					destination: 'doc'
-				}
-			}
-		},
-		jscs: {
-			src: ['src/**/*.js', 'Gruntfile.js', 'example/app.js'],
-			options: {
-				config: '.jscs.json'
-			}
-		},
-		version: {
-			js: {
-				options: {
-					prefix: '@version\\s*'
-				},
-				src: ['src/**/*.js']
-			},
-			json: {
-				options: {
-					prefix: '"version":\\s"*'
-				},
-				src: ['bower.json']
-			}
-		}
-	});
+  
+  require('time-grunt')(grunt);
+  
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies',
+    config: 'package.json',
+    pattern: ['grunt-*']
+  });
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'src/**/*.js',
+        'example/app.js',
+        'Gruntfile.js'
+      ]
+    },
+    uglify: {
+      options: {
+        mangle: false,
+        sourceMap: true,
+        sourceMapName: 'dist/processing-yeoman.min.map'
+      },
+      myTarget: {
+        files: {
+          'dist/processing-yeoman.min.js': ['src/processing-yeoman.js']
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['src/processing-yeoman.js'], dest: 'dist/processing-yeoman.js'},
+          {src: ['dist/processing-yeoman.min.js'], dest: 'example/processing-yeoman.min.js'},
+          {src: ['dist/processing-yeoman.min.map'], dest: 'example/processing-yeoman.min.map'},
+          {src: ['bower_components/jquery/jquery.min.js'], dest: 'example/jquery.min.js'},
+          {src: ['bower_components/jquery/jquery.min.map'], dest: 'example/jquery.min.map'}
+        ]
+      }
+    },
+    jsdoc : {
+      dist : {
+        src: ['src/**/*.js'],
+        options: {
+          destination: 'doc'
+        }
+      }
+    },
+    jscs: {
+      src: ['src/**/*.js', 'Gruntfile.js', 'example/app.js'],
+      options: {
+        config: '.jscs.json'
+      }
+    },
+    version: {
+      js: {
+        options: {
+          prefix: '@version\\s*'
+        },
+        src: ['src/**/*.js']
+      },
+      json: {
+        options: {
+          prefix: '"version":\\s"*'
+        },
+        src: ['bower.json']
+      }
+    }
+  });
+  
+  //grunt.loadNpmTasks('grunt-contrib-watch');
 
-	// Register tasks
-	grunt.registerTask('build', [
-		'version:js',
-		'version:json',
-		'jshint',
-		'jscs',
-		'uglify',
-		'copy:main',
-		'jsdoc:dist'
-	]);
-	
-	grunt.registerTask('serve', [
-		'build'
-	]);
+  // Register tasks
+  grunt.registerTask('build', [
+    'version:js',
+    'version:json',
+    'jshint',
+    'jscs',
+    'uglify',
+    'copy:main',
+    'jsdoc:dist'
+  ]);
+  
+  grunt.registerTask('serve', ['build']);
+  grunt.registerTask('default', ['build']);
 };
